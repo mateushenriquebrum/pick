a3a6da593d6630898324e7008db9abb7aca14fae
Mateus Henrique Brum <mateushenriquebrum@gmail.com>
2021-03-10 15:52:28 +0000

    test refactory

--- src/test/kotlin/domain/usecase/SetFreeSlotOnCalendarTest.kt
+++ src/test/kotlin/domain/usecase/SetFreeSlotOnCalendarTest.kt
@@ -6,23 +6,22 @@ import domain.slot.Free
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime.now
 
 class SetFreeSlotOnCalendarTest {
+    private val now = now()
+    private val interviewer = "interviewer@gmail.com"
+
     @Test
     fun `Interviewer should get a Confirmation when set a Free Slot on its Calendar`() {
-        val now = now()
         val rep:InterviewerRepository = mockk(relaxed = true);
-        val setFree = SetFreeSlotCalendar(rep)
-        var result = setFree.execute("interviewer@gmail.com", now, 10)
-        when(result) {
+        when(val result = SetFreeSlotCalendar(rep).execute(interviewer, now, 10)) {
             is Either.Right -> {
-                assertEquals(result.b, Confirm("interviewer@gmail.com", now, 10))
-                verify { rep.setFreeSlot("interviewer@gmail.com", Free(now, 10, "interviewer@gmail.com")) }
+                assertEquals(result.b, Confirm(interviewer, now, 10))
+                verify { rep.setFreeSlot(interviewer, Free(now, 10, interviewer)) }
             }
             is Either.Left -> fail()
         }
@@ -30,13 +29,9 @@ class SetFreeSlotOnCalendarTest {
 
     @Test
     fun `Interviewer should get a Negation when set a existent Free Slot on its Calendar`() {
-        val now = now()
-        val interviewer = "interviewer@gmail.com"
         val rep:InterviewerRepository = mockk(relaxed = true);
         every { rep.getInterviewerCalendar(any()) } returns Calendar(Free(now, 10, interviewer))
-        val setFree = SetFreeSlotCalendar(rep)
-        var result = setFree.execute(interviewer, now, 10)
-        when(result) {
+        when(val result = SetFreeSlotCalendar(rep).execute(interviewer, now, 10)) {
             is Either.Right -> fail()
             is Either.Left -> {
                 assertEquals(result.a, Invalid("Slot already set"))
